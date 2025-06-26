866a2c03a1c82c3d7c6f450fb9fca8335fc21425
EliasAlgusto <eliasagcampos@gmail.com>
2025-06-26 11:01:14 -0300

    Add files via upload
    Atividade 14(T2)

---
+++ ex14.py
@@ -0,0 +1,15 @@
+
+pares = 0
+impares = 0
+
+
+for _ in range(10):  # Loop para solicitar 10 números
+    num = int(input("Digite um número inteiro: "))
+    if num % 2 == 0:
+        pares += 1
+    else:
+        impares += 1
+
+
+print(f"Quantidade de números pares: {pares}")
+print(f"Quantidade de números ímpares: {impares}")
