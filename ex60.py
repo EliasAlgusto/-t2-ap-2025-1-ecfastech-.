cff0634bb4805fa761f6a091045c255d403e8146
EliasAlgusto <eliasagcampos@gmail.com>
2025-07-03 16:06:00 -0300

    Add files via upload
    Atividade (T2)

---
+++ ex60.py
@@ -0,0 +1,7 @@
+
+def inverter_string(texto):
+    texto_invertido = texto[::-1]
+    print(f" Texto invertido: {texto_invertido}"
+
+entrada = input("Digite uma frase ou palavra: ")
+inverter_string(entrada)
